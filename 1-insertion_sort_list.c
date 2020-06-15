636d9cf9de6bff33fc732bd116e9de286ae034e4
Taha Elleuch <tahaeleuch@gmail.com>
2020-06-15 11:34:08 +0000

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order
- * using the Insertion sort algorithm
+ * insertion_sort_list - sorts a doubly linked list of integers in
+ * ascending order using the Insertion sort algorithm
  * @list: doubly linked list of integers
 */
 void insertion_sort_list(listint_t **list)
